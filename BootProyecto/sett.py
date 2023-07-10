7fc607cf2924c76b3802cfe3b0fb732d6360c442
Pedro Alberto García Maldonado <61610557+pedroG98-coder@users.noreply.github.com>
2023-07-10 13:48:06 -0600

    Add files via upload
    Proyecto arriba

---
+++ BootProyecto/sett.py
@@ -0,0 +1,6 @@
+token = 'GoTechStore'
+
+whatsapp_token = 'EAACcNOVMnQwBAMoh15t6nRxKdPETadGOTHyP8Uhcr1Hy5JW66bJuiLOf4ORYKsAralxMgO3NBStFSjzLyrFVkKnLkQZBJVkX0QUrz9LPEfIZAvhlP1yfpd1Pys62dIeLDuPKRPOrUjQiXybCuJ64qj6DOYPtRUXAhYK9TRi0jR3XymjrI2'
+
+whatsapp_url = 'https://graph.facebook.com/v17.0/115997168218803/messages'
+
